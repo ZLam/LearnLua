f92f5b82203210de125ac7fd9e0f00a1e8303d94
ZLam <578058407@qq.com>
2023-08-21 20:24:02 +0800

    update...

--- HelloWorld/res/script/test_upvalue_02.lua
+++ HelloWorld/res/script/test_upvalue_02.lua
@@ -4,3 +4,13 @@ local function f()
         u = v
     end
 end
+
+-- local u, v, w
+-- local function f()
+--     local function g()
+--         u = v
+--         local function h()
+--             w = 0
+--         end
+--     end
+-- end
